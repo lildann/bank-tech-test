c6170434f00129af364a3cacb6e6f1d21d4f13af
Lilly Daniell <daniell.lilly@gmail.com>
2021-10-26 17:02:56 +0100

    Balance changes with multiple deposits and whitdrawals

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -11,10 +11,12 @@ describe Account do
 
   it "should debit account with a specified amount of money" do
     expect(account.debit(500, "14/01/2023")).to eq(-500)
+  end
+
+  it "should credit and debit account, changing the running balance" do
     account.credit(1000, "10/01/2023")
     account.credit(2000, "13/01/2023")
     expect(account.debit(500, "14/01/2023")).to eq(2500)
   end
 
-  it "should "
 end
