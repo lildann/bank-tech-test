7c8b635efb3a87bd102cd3d8ef42e38808545e99
Lilly Daniell <daniell.lilly@gmail.com>
2021-11-03 09:05:21 +0000

    Refactor return values of credit and debit methods and update tests

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,18 +5,21 @@ describe Account do
   let(:account) { Account.new }
   
   it "should credit account with a specified amount of money" do
-    expect(account.credit_balance(1000, "10/01/2023")).to eq(1000)
-    expect(account.credit_balance(2000, "13/01/2023")).to eq(3000)
+    account.credit_balance(1000, "10/01/2023")
+    expect(account.balance).to eq(1000)
+    account.credit_balance(2000, "13/01/2023")
+    expect(account.balance).to eq(3000)
   end
 
   it "should debit account with a specified amount of money" do
-    expect(account.debit_balance(500, "14/01/2023")).to eq(-500)
+    account.debit_balance(500, "14/01/2023")
+    expect(account.balance).to eq(-500)
   end
 
   it "should credit and debit account, changing the running balance" do
     account.credit_balance(1000, "10/01/2023")
-    account.credit_balance(2000, "13/01/2023")
-    expect(account.debit_balance(500, "14/01/2023")).to eq(2500)
+    account.debit_balance(950, "13/01/2023")
+    expect(account.balance).to eq(50)
   end
 
   it "should print a full statement showing the banner, date, amount credited / debited, and the balance" do
